ab476d37168f97a34cded125a7716984aa80e892
Nika Tomadze <nika.tomadze@gmail.com>
2014-12-09 13:48:16 +0400

    tabs converted to spaces

--- app/app.js
+++ app/app.js
@@ -1,23 +1,23 @@
 var app = angular.module('MayBelline', []);
 
-app.factory('instagram', function($http){
-	var tag = 'goextremeno';
+app.factory('instagram', function($http) {
+    var tag = 'goextremeno';
 
-	return {
-		byHashtag: function(callback){
+    return {
+        byHashtag: function(callback) {
             var insUrl = 'https://api.instagram.com/v1/tags/' + tag + '/media/recent?client_id=74a1447737884721bc993537dab854de&callback=JSON_CALLBACK';
 
-            $http.jsonp(insUrl).success(function(response){
+            $http.jsonp(insUrl).success(function(response) {
                 callback(response.data);
             });
-		}
-	};
+        }
+    };
 });
 
-app.controller('instagramCtrl', function ($scope, instagram){
-	$scope.pics = [];
+app.controller('instagramCtrl', function($scope, instagram) {
+    $scope.pics = [];
 
-	instagram.byHashtag(function(data){
-		$scope.pics = data;
-	});
+    instagram.byHashtag(function(data) {
+        $scope.pics = data;
+    });
 });
